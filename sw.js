553ae65ed7477f1b631b3654e4a62db8cc696045
Slashgear <Slashgear@users.noreply.github.com>
2020-11-26 22:34:53 +0000

    Deploying to master from  @ a761d7b1c7423d7d04714206c2cbe8035454dc98 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9e38518fcc5a3d6242a2.js"
+    "url": "webpack-runtime-f69f1eaaa842eb658dc8.js"
   },
   {
     "url": "styles.aa5b66621eae5c67c2b4.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e6b5798b2b0d459bff241915bf558f1d"
+    "revision": "a7625c63d4695241ec7b9a05b0bebba3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
