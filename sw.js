ec896346d229c80278f021511091a28031f3c659
Slashgear <Slashgear@users.noreply.github.com>
2022-04-24 20:53:35 +0000

    Deploying to master from @ Slashgear/slashgear.github.io@ab97a81d1ff1b16369a95675d50e37b5100598a0 🚀

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7ce5fcb2adab72cc6d6d.js"
+    "url": "webpack-runtime-b65df8954a096657df53.js"
   },
   {
-    "url": "styles.c2a66ed9064d216d4969.css"
+    "url": "styles.c41abb35059ebfd0bdf9.css"
   },
   {
-    "url": "framework-4d90cb9cb9344c3bd0c0.js"
+    "url": "framework-0d17df5b87da0c58418f.js"
   },
   {
-    "url": "app-7b1e8e9f306cfadc0120.js"
+    "url": "app-aa51affcb26119fe3660.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "38b13543775d810551bb5ff72e5de1c4"
+    "revision": "774a5176ae68452891478e92afb3098e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-be75cc33cf1d2b05b73d.js"
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-7b1e8e9f306cfadc0120.js`))) {
+  if (!resources || !(await caches.match(`/app-aa51affcb26119fe3660.js`))) {
     return await fetch(event.request)
   }
 
